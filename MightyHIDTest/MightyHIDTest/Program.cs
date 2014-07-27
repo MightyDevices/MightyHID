57c26d8474a4453adc8c71c72616503ed08e95b9
twatorowski <tomasz.watorowski@gmail.com>
2014-07-27 13:40:57 +0200

    Demonstration of MightyHID
    Simple piece of software that demonstrates how to use MightyHID in your
    projects.

---
+++ MightyHIDTest/MightyHIDTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MightyHID;
+
+namespace MightyHIDTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* hello, world! */
+            Console.WriteLine("List of USB HID devices:");
+            /* browse for hid devices */
+            var devs = HIDBrowse.Browse();
+            /* display VID and PID for every device found */
+            foreach (var dev in devs) {
+                Console.WriteLine("VID = " + dev.Vid.ToString("X4") +
+                    " PID = " + dev.Pid.ToString("X4") + 
+                    " Product: " + dev.Product);
+            }
+            
+            /* try to connect to first device */
+            if (devs.Count > 0) {
+                /* new device */
+                HIDDev dev = new HIDDev(); 
+                /* connect */
+                dev.Open(devs[0]);
+                /* an example of hid report, report id is always located 
+                 * at the beginning of every report. Here it was set to 0x01.
+                 * adjust this report so it does meet your hid device reports */
+                byte[] report = new byte[32]; report[0] = 0x01;
+                /* send report */
+                dev.Write(report);
+                /* get response */
+                dev.Read(report);
+            }
+
+            /* bye bye! */
+            Console.Read();
+        }
+    }
+}
